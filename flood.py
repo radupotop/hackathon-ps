0ebf427e162365eb96d742a7d79a6d8fea8653fc
Radu Potop <radu.potop@polestarglobal.com>
2019-12-06 17:57:23 +0000

    Refactor flood fill to use an adaptive approach

--- flood.py
+++ flood.py
@@ -12,20 +12,46 @@ port_seedpoint_map = (
 )
 
 
-def fill_image(name, seed_point):
-    # Load
+def fill_image(name, seed_point, colors=2):
+    print(f'Processing image: {name}')
+
+    img = quantize_and_fill(name, seed_point, colors)
+    num_px = get_num_pixels(img)
+    r_histo = get_r_histo(img)
+
+    if r_histo[-1] / num_px > 0.9:
+        print(f'Redoing with {colors+1} colours...')
+        img = fill_image(name, seed_point, colors=colors+1)
+
+    return img
+
+
+def quantize_and_fill(name, seed_point, colors=2):
+    """
+    Load and transform.
+    """
     img = Image.open(f'images/{name}.png')
 
-    # Transform
-    img = img.quantize(colors=3, dither=Image.NEAREST, method=Image.MEDIANCUT).convert('L').convert('RGB')
+    img = img.quantize(colors=colors, dither=Image.NEAREST, method=Image.MEDIANCUT).convert('L').convert('RGB')
+    ImageDraw.floodfill(img, xy=seed_point, value=(255,0,0))
 
-    # img = img.filter(filter=ImageFilter.FIND_EDGES)
-    ImageDraw.floodfill(img, xy=seed_point, value=(127,0,0,0))
+    return img
 
-    print('Image size: ', img.getbbox())
+def get_num_pixels(img):
+    num_pixels = img.width * img.height
+    return num_pixels
 
+def get_r_histo(img):
+    r,g,b = img.split()
+    r_histo = r.histogram()
+    return r_histo
+
+def save_img(img, name):
+    print(f'Saving {name}')
     img.save(f'output/{name}.png')
 
 
 for port in port_seedpoint_map:
-    fill_image(*port)
+    save_img(fill_image(*port), port[0])
+
+# img = fill_image(*port_seedpoint_map[0])
