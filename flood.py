40d8d43311aa0166a731c264f08dc47a34f391bb
Radu Potop <radu.potop@polestarglobal.com>
2019-12-06 13:47:41 +0000

    Added comments

--- flood.py
+++ flood.py
@@ -1,3 +1,10 @@
+"""
+1. Find a seed point (the original port coordinates).
+2. Do an quantitize and fill on the map to detect edges.
+    - This detects points of interest such as terminals and berths.
+3. Put bounding boxes around those POIs and built Rtrees in PostGIS
+"""
+
 from pathlib import Path
 
 from PIL import Image, ImageDraw, ImageFilter
@@ -7,12 +14,12 @@ img = Image.open('images/southampton_colour.png')
 
 # Transform
 img = img.quantize(colors=4, dither=Image.NEAREST, method=Image.MEDIANCUT).convert('RGB')
-# img = img.filter(filter=ImageFilter.EDGE_ENHANCE_MORE)
 
 # Port coordinate
 seed_point = (900, 850)
 
-out = ImageDraw.floodfill(img, xy=seed_point, value=(122,0,0,255))
+# img = img.filter(filter=ImageFilter.FIND_EDGES)
+ImageDraw.floodfill(img, xy=seed_point, value=(127,0,0,0))
 
 print('Image size: ', img.getbbox())
 
